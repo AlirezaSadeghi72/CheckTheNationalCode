6edb97d723c9356d632518549c8c751785bc345c
Alireza Sadeghi <dev.AlirezaSadeghi@gmail.com>
2020-06-07 10:29:59 +0430

    Add project files.

---
+++ CheckTheNationalCode/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CheckTheNationalCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(12, 12);
+            this.txtCode.MaxLength = 10;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(181, 20);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtCode.TextChanged += new System.EventHandler(this.txtCode_TextChanged);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblStatus.Location = new System.Drawing.Point(0, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(217, 51);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "label1";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 86);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtCode);
+            this.Name = "Form1";
+            this.Text = "برسی صحت کد ملی";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ CheckTheNationalCode/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckTheNationalCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCode_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCode.Text.Length == 0)
+            {
+                lblStatus.Text = "لطفا کد ملی را وارد کنید";
+                lblStatus.ForeColor = Color.Black;
+            }
+            else
+            {
+                SetLabalStatus(CheckCode(txtCode.Text));
+            }
+        }
+
+        public bool CheckCode(string TextNationalCode)
+        {
+            try
+            {
+                var AllDigitEqual = new[]
+                {
+                    "0000000000",
+                    "1111111111",
+                    "2222222222",
+                    "3333333333",
+                    "4444444444",
+                    "5555555555",
+                    "6666666666",
+                    "7777777777",
+                    "8888888888",
+                    "9999999999"
+                };
+                if (AllDigitEqual.Contains(TextNationalCode))
+                    return false;
+
+                List<int> multiplication = new List<int>();
+                int i = 0;
+                foreach (var item in TextNationalCode.ToCharArray())
+                {
+                    int num = Convert.ToInt32(item.ToString()) * (10 - i);
+                    multiplication.Add(num);
+                    i++;
+                }
+
+                var sum = multiplication.GetRange(0, 9).Sum();
+                var c = sum % 11;
+
+                return (((c < 2) && (multiplication[9] == c)) || ((c >= 2) && ((11 - c) == multiplication[9])));
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void SetLabalStatus(bool Status)
+        {
+            if (Status)
+            {
+                lblStatus.Text = "کد ملی وارد شده درست است";
+                lblStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblStatus.Text = "کد ملی وارد شده اشتباه است";
+                lblStatus.ForeColor = Color.Red;
+            }
+        }
+    }
+}
